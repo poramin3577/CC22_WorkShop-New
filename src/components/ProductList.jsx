ffceef29a6a0913e99f7bf14e30b78062ba5ca9b
poramin3577 <poramin3577@gmail.com>
2026-09-17 16:49:23 +0700

    Add fetch

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from './ProductCard'
 import axios from "axios";
 
 function ProductList() {
-
+  const [products, setProduct] = useState([])
+  const fetchProduct =() =>{
+    axios.get('https://fakestoreapi.com/products')
+    .then(response => {
+      console.log(response)
+    })
+  }
+  useEffect(()=>{
+    fetchProduct()
+  },[])
   return (
     <div className='bg-amber-300 w-2/3 flex items-start ps-8 gap-4 p-4 flex-wrap overflow-auto'>
+      <pre>{JSON.stringify()}</pre>
       <ProductCard />
       <ProductCard />
       <ProductCard />
