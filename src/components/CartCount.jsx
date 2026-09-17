c366e7aee95125b74701a6292ce6113a19d6969b
poramin3577 <poramin3577@gmail.com>
2026-09-17 13:47:55 +0700

    Third Commit

--- src/components/CartCount.jsx
+++ src/components/CartCount.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function CartCount() {
   return (
-    <div>
-      CartCount
+    <div className='relative'>
+    <div className='w-15'>
+      <img src='https://www.svgrepo.com/show/529452/cart-large-4.svg' alt='Cart'/>
+    </div>
+    <div className='text-violet-500 bg-white w-5 h-5 absolute top-2 right-0  flex justify-center items-center rounded-xl'>9</div>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import CartCount from './CartCount'
 
 function Header() {
   return (
-    <div className='h-1/12'>
-      Header
+    <div className='h-1/12 flex justify-between items-center px-4'>
+      <div className='h-5/5  flex items-center gap-2'>
+          <img className="h-full" src='https://www.svgrepo.com/show/501826/shop.svg' alt='Shop' />
+          <h3 className='text-3xl font-bold text-blue-600'>CC22 SHOP</h3>
+      </div>
+      <CartCount />
     </div>
   )
 }
