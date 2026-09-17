3584bf4538102995e8fb1c96ce417c370a3c7b66
poramin3577 <poramin3577@gmail.com>
2026-09-17 17:45:45 +0700

    Sent props

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function ProductCard() {
+function ProductCard({product}) {
   return (
-    <div className="card bg-base-100 w-50 shadow-sm">
-  <figure>
+    <div className="card bg-base-100 w-50 h-[400px] shadow-sm">
+  <figure className=''>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
+      src={product.image}
+      alt={product.title}
+      />
   </figure>
   <div className="card-body">
     <h2 className="card-title">Card Title</h2>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,7 +7,8 @@ function ProductList() {
   const fetchProduct =() =>{
     axios.get('https://fakestoreapi.com/products')
     .then(response => {
-      console.log(response)
+      console.log(response.data)
+      setProduct(response.data)
     })
   }
   useEffect(()=>{
@@ -15,16 +16,10 @@ function ProductList() {
   },[])
   return (
     <div className='bg-amber-300 w-2/3 flex items-start ps-8 gap-4 p-4 flex-wrap overflow-auto'>
-      <pre>{JSON.stringify()}</pre>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      <pre>{JSON.stringify(products[0],null,2)}</pre> 
+      {products.map(product=>(
+        <ProductCard key={product.id} product={product}/>
+      ))}
     </div>
   )
 }
