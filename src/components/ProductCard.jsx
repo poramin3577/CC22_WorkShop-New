390c5b23344e6e18ad0dd9878000ded256bb6589
poramin3577 <poramin3577@gmail.com>
2026-09-20 05:16:57 +0700

    Card-1

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 function ProductCard({product}) {
   return (
-    <div className="card bg-base-100 w-50 h-[400px] shadow-sm">
-  <figure className=''>
+    <div className="card bg-base-100 w-60 h-[420px] shadow-sm">
+  <figure className='h-48  shrink-0 p-4'>
     <img
+      className='w-full h-full object-contain mt-2 p-2'
       src={product.image}
       alt={product.title}
       />
   </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+  <div className="card-body flex-none gap-5">
+    <h2 className="card-title text-[20px] line-clamp-1">{product.title}</h2>
+    <p className='line-clamp-3 text-base leading-7'>{product.description}</p>
+    <div className="flex justify-between items-center mt-auto">
+      <p className='text-red-400 text-xl'>{product.price} THB</p>
+      <button className="btn btn-sm btn-primary">Add to Cart</button>
     </div>
   </div>
 </div>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,7 +7,6 @@ function ProductList() {
   const fetchProduct =() =>{
     axios.get('https://fakestoreapi.com/products')
     .then(response => {
-      console.log(response.data)
       setProduct(response.data)
     })
   }
@@ -16,9 +15,9 @@ function ProductList() {
   },[])
   return (
     <div className='bg-amber-300 w-2/3 flex items-start ps-8 gap-4 p-4 flex-wrap overflow-auto'>
-      <pre>{JSON.stringify(products[0],null,2)}</pre> 
-      {products.map(product=>(
-        <ProductCard key={product.id} product={product}/>
+      {/* <pre>{JSON.stringify(products[0],null,2)}</pre>  */}
+      {products.map(el=>(
+        <ProductCard key={el.id} product={el}/>
       ))}
     </div>
   )
