c5ec3a276c5a1c66206814d8746ff3bd5b6b7e07
poramin3577 <poramin3577@gmail.com>
2026-09-22 15:15:39 +0700

    CartCount with State

--- src/components/CartCount.jsx
+++ src/components/CartCount.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function CartCount() {
+function CartCount({itemCount}) {
   return (
     <div className='relative'>
     <div className='w-15'>
       <img src='https://www.svgrepo.com/show/529452/cart-large-4.svg' alt='Cart'/>
     </div>
-    <div className='text-violet-500 bg-white w-6 h-6 absolute top-1 left-10  flex justify-center items-center rounded-xl'>100</div>
+    <div className='text-violet-500 bg-white w-6 h-6 absolute top-1 left-10  flex justify-center items-center rounded-xl'>{itemCount}</div>
     </div>
   )
 }
